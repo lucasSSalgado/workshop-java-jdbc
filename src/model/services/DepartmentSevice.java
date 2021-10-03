8e498a3aefa71145aa4ff9840dc32a38174a8258
Lucas Sodré Salgado <lucssslucsss@gmail.com>
2021-10-02 22:19:07 -0300

    saving new department

--- src/model/services/DepartmentSevice.java
+++ src/model/services/DepartmentSevice.java
@@ -13,5 +13,14 @@ public class DepartmentSevice {
 	public List<Department> findAll(){
 		return dao.findAll();
 	}
+	
+	public void saveOurUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 
 }
